6b70f04eee1c17bba75d8c4bdb7a2a7ff58ba672
marcus <marcus.rodrigues10@hotmail.com>
2021-07-14 13:50:26 -0300

    iniciando projeto

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,43 @@ const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
+const Box = styled.div`
+  background: #ffff;
+  border-radius: 8px;
+`;
+
+const MainGrid = styled.main`
+  display: grid;
+  grid-gap: 10px;
+  padding: 16px;
+
+  @media(min-width: 860px) {
+    grid-template-areas: 'profileArea welcomeArea profileRelationsArea';
+    grid-templates-columns: 160px 618px 312px;
+  }
+`
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <MainGrid> 
+      <div style={{ gridArea: 'profileArea' }}>
+        <Box>
+          Imagem
+        </Box>
+      </div>
+      <div style={{ gridArea: 'welcomeArea' }}>
+        <Box>
+          Bem-Vindo
+        </Box>
+      </div>
+      <div style={{ gridArea: 'profileRelationsArea' }}>
+        <Box>
+          Pessoas da comunidade
+        </Box>
+        <Box>
+          Comunidade
+        </Box>
+      </div>
+    </MainGrid>
+  )
 }
